c741e6ded4fa8946b96a0d06a1cc1a8b003951a7
Alex <alexanderhuxel@gmail.com>
2023-01-18 22:19:11 +0100

    implement backend service and create simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fizzbuzz';
+
+  getValueofFormAndFillList(val: any) {
+    this.backend.getList(val.number);
+  }
+
+  constructor(public backend: BackendService) {}
 }
